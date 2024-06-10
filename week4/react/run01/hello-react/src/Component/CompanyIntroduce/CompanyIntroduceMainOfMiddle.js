b60dafe1ac8b369bcfc6dbf96e5e90331cc09392
wooest <htpapa9645@gmail.com>
2024-06-10 23:23:14 +0900

    2024 06 10 2번쩨 리액트 회사소개 작업중

--- week4/react/run01/hello-react/src/Component/CompanyIntroduce/CompanyIntroduceMainOfMiddle.js
+++ week4/react/run01/hello-react/src/Component/CompanyIntroduce/CompanyIntroduceMainOfMiddle.js
@@ -1,8 +1,38 @@
 import React from "react";
 
-const CompanyIntroduceMainOfMiddle = () => {
+const MyButton = ({name, color}) => {
+    return (
+        <button style = {
+                            {
+                                width :'25%', 
+                                backgroundColor:color,
+                            }
+                        }>{name}</button>
+    )
+}
 
-    return "";
+const MyIntroduceText = () => {
+    return (
+        <div style={{marginTop:'10px', border:'solid 1px', padding:'10px'}}>
+            소개글
+        </div>
+    )
+}
+
+const CompanyIntroduceMainOfMiddle = () => {
+    return (
+        <>
+            <div>
+                <MyButton name="사원1" color="lightpink"></MyButton>
+                <MyButton name="사원2" color="skyblue"></MyButton>
+                <MyButton name="사원3" color="lightgreen"></MyButton>
+                <MyButton name="사원4" color="yellow"></MyButton>
+            </div>
+            <div>
+               <MyIntroduceText></MyIntroduceText>
+            </div>
+        </>
+    );
 }
 
 export default CompanyIntroduceMainOfMiddle;
